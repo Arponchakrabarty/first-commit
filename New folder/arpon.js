ec2188d05ca00823c455ec91fa7600e106a10305
Arponchakrabarty <arponchakrabarty71@gmail.com>
2026-08-02 12:29:37 +0600

    update

---
+++ New folder/arpon.js
@@ -0,0 +1,73 @@
+console.log("helloworld");
+
+
+var orange =70
+console.log(orange)
+var apple = 100
+console.log(apple)
+var mango = 80
+console.log(mango)
+
+
+
+var amrKaseAse =1500
+var cafe = 1800
+var remoteCar =1700
+var coke= 110
+
+
+if (amrKaseAse>=cafe){
+    console.log("cafe t jaimu")
+}
+else{
+    console.log("jaitam ny")
+}
+
+
+if(amrKaseAse>=remoteCar){
+    console.log("car kinmu")
+}
+
+else if(amrKaseAse>=cafe)(
+    console.log("cafe t jaimu")
+)
+else if(amrKaseAse>=coke){
+    console.log("chocolate khaimu")
+}
+
+
+var ab = 0.1*0.5*5.25
+console.log(parseFloat(ab. toFixed(4)))
+
+var cd ="45*50"
+console.log(parseInt(cd))
+
+
+console.log( typeof number)
+
+var string = ["abcieow","fhfhfhfhf","siusadf","doisaudfh"]
+console.log(string)
+
+var boolean = [true,false]
+console.log(boolean)
+
+var undefined = undefined
+
+var nul = null
+var number= [123333456, 15425415,15412,45548, 1525,100000]
+
+
+
+console.log(number)
+number.push(111111111) //add number in last side in array
+console.log(number)
+console.log(number.indexOf(1525))
+number.pop() // remove number/character from the last side in array
+console.log(number)
+number.pop()
+console.log(number)
+number.shift() // remove number or character from first side  in array
+console.log(number)
+number.unshift(120) // add number or character in first side  in array
+console.log(number)
+
