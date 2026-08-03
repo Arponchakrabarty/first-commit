f43ad0b46bbf480ee81999436fec932c21472524
Arponchakrabarty <arponchakrabarty71@gmail.com>
2026-08-03 14:15:05 +0600

    update

---
+++ New folder/loop.js
@@ -0,0 +1,44 @@
+// var std= [80, 75, 35, 59, 78, 93, 84, 48,50,90,79,100,53]
+// console.log(std)
+
+
+// for(var i = 0 ; std.length>i ; i++){
+//     if(std[i]>=80)
+//         console.log(`student${i+1} paise`,"A+")
+//     else if(std[i]>70)
+//         console.log(`student${i+1} paise`,"A")
+//     else if(std[i]>60)
+//         console.log(`student${i+1} paise`,"A-")
+//     else if(std[i]>50)
+//         console.log(`student${i+1} paise`,"B")
+//       else if(std[i]>40)
+//         console.log(`student${i+1} paise`,"C")
+//       else if(std[i]>33)
+//         console.log(`student${i+1} paise`,"D")
+//       else if(std[i]<33)
+//         console.log(`student${i+1} paise`,"F")
+// }
+
+
+
+var std1 = [100,90,48,30,38,78,74,54,52,51,23,89,59,54]
+console.log(std1)
+
+for(var i=0; std1.length>i; i++){
+        if(std1[i]>=80)
+    console.log(`student${i+1} paise`,"A+")
+else if(std1[i]>70)
+    console.log(`student${i+1} paise`,"A")
+else if(std1[i]>60)
+    console.log(`student${i+1} paise`,"A-")
+else if(std1[i]>50)
+    console.log(`student${i+1} paise`,"B")
+else if(std1[i]>40)
+    console.log(`student${i+1} paise`,"C")
+else if(std1[i]>33)
+    console.log(`student${i+1} paise`,"D")
+else if(std1[i]<33)
+    console.log(`student${i+1} paise`,"F")
+}
+
+
