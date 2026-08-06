aad3352a79cccaf2dd007e8d91339d2ed5bf4d0f
Arponchakrabarty <arponchakrabarty71@gmail.com>
2026-08-06 17:11:02 +0600

    update

---
+++ New folder/all.js
@@ -0,0 +1,131 @@
+
+// variable and data type 
+
+// var favNum= 10
+// console.log(typeof favNum)
+// var nul = null
+// console.log(nul)
+// var number= undefined
+// console.log(number)
+// var i = "100"
+// console.log(typeof i)
+// var A = 100
+// console.log(typeof A)
+// var B = true
+// console.log(typeof B)
+// var c = null
+// console.log(typeof c)
+// var D = undefined
+// console.log(D)
+
+
+
+// string
+
+// var name ="Arpon chakrabarty"
+// console.log(name)
+// console.log(name.length)
+// var name ="Arpon chakrabarty"
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// // console.log(name[0])
+// console.log(name[16])
+// var a ="Arpon"
+// var b = "chakrabarty"
+// console.log(a,b)
+// var E = `My name is John and I am 20 years old`
+// console.log(E)
+
+
+// number 
+
+// var a= 20
+// var b= 10
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// a +=10
+// console.log(a)
+// b -=5
+// console.log(b)
+
+// 1 or modulas ar 2 parsi na 
+
+
+// boolean
+
+// var isLoggedIn = true
+// var isAdmin = false
+
+// console.log(isLoggedIn)
+// console.log(!isLoggedIn)// bujsi na 
+// console.log(isLoggedIn && isAdmin)// bujsi na 
+// console.log(isLoggedIn || isAdmin)// bujsi na 
+
+// Array 
+
+// var fruits =["Apple", "Banana", "Orange"]
+// console.log(fruits)
+// console.log(fruits[0])
+// console.log(fruits[2])
+// fruits.push("Mango")
+// console.log(fruits) 
+// fruits.pop()
+// console.log(fruits) 
+// fruits[1]="Grapes"
+// console.log(fruits) 
+
+
+// Object
+
+// var std= {name:"Rahim",
+//         age:18,
+//         city:"Dhaka"
+// }
+// console.log(std)
+// console.log(std.name)
+// console.log(std.city)
+// std.phn = "I phone";
+// console.log(std)
+// std.age= 20
+// console.log(std)
+// delete std.city
+// console.log(std)
+
+
+// mix practice
+
+// var phn = { brand: "iphn",
+//             model: "s26ultra",
+//             price:226000,
+//             color:" blue",
+//             isAvailable: true 
+// }
+// console.log(phn)
+
+var book= { name:"Physics",
+            forClass:10,
+            price:200,
+
+ }
+ console.log(book)
+
+
+
+// type checking
+
+// var a= "Hello"
+//  console.log(typeof a)
+// var b = 100
+//   console.log(typeof b)
+// var c = true
+//  console.log(typeof c)
+//  var d = undefined
+//   console.log(typeof d)
+// var e = null
+//  console.log(typeof e)
+// var f= [1,2,3]
+//  console.log(typeof f)
+// var g = {car:1000000, bike:20000}
+//  console.log(typeof g)    
